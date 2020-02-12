ff49186693461bb1cc0d0b8604324364df89ff74
JuliusDeng <1576554007@qq.com>
2020-02-12 13:31:22 +0800

    Vue 高级知识点

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container p-5">
+		<h3>你可知今天是：</h3>
+		<!-- <on-emit></on-emit> -->
+		
+		<!-- <div class="alert alert-primary" v-show="show">
+			消息提示
+		</div>
+		<provide></provide> -->
+		
+		<demo></demo>
+		<!-- <time-xia></time-xia> -->
+		<!-- <button class="btn btn-success" @click="clickEvent">app.vue的点击事件</button> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+	// import demo from "@/components/provide-test/demo.vue"
+	import demo from '@/components/on-emit/demo.vue'
+	// import demo from '@/components/provide-inject/demo.vue'
+	// import demo from '@/components/directive/demo.vue'
+	// import demo from "@/components/directive/v-menu.vue"
+	// import demo from '@/components/nextTick/demo.vue'
+	// import demo from '@/components/sync/demox.vue'
+	// import demo from '@/components/mixin/demo.vue'
+	// import timeXia from "@/components/demo/time.vue"
 
 export default {
+	provide() {
+		return {
+			$_app: this
+		}
+	},
+	// provide() {
+	// 	return {
+	// 		show: this.show,
+	// 		showHide: this.showHide
+	// 	}
+	// },
   name: 'app',
   components: {
-    HelloWorld
-  }
+    // provide,
+		demo,
+		// timeXia
+  },
+	data() {
+		return {
+			// show: false
+		}
+	},
+	methods: {
+		// showHide(data) {
+		// 	this.show = data
+		// }
+		clickEvent() {
+			this.$emit('test', {
+				value: 'app.vue的点击事件'
+			})
+		}
+	}
+	
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,37 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 全局混入mixin
+import mixin from "@/components/mixin/mixin.js"
+Vue.mixin(mixin)
+
+Vue.directive("skeleton", {
+	bind(el, binding) {
+		// console.log(binding, '---bind');
+		el.style.backgroundColor = "#eeeeee",
+		el.style.color = "#eeeeee"
+	},
+	update(el, binding) {
+		// console.log('---update');
+		console.log(binding, '--binding');
+		// console.log(binding.value);
+		// console.log(binding.modifiers, '--update el');
+		el.style.backgroundColor = "";
+		el.style.color = "";
+		for (let key in binding.modifiers) {
+			switch (key){
+				case "img":
+				el.src = binding.value
+					break;
+				case "text":
+				el.innerHTML = binding.value
+					break;
+			}
+		}
+	}
+})
+
+
 Vue.config.productionTip = false
 
 new Vue({
